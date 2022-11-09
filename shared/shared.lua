fad63be360e2cf4d7596e36d7ae33754ac079ab0
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-11-09 15:29:51 -0500

    cleaned up config

--- shared/shared.lua
+++ shared/shared.lua
@@ -4,20 +4,21 @@ Config.Core = 'qb-core'
 Config.Target = 'qb-target'
 Config.Fuel = 'LegacyFuel'
 
+Config.NotificationStyle = 'phone'
+Config.TowTruck = 'flatbed'
+
+-- PHONE CONFIG --
 Config.RenewedPhone = true -- If you use Renewed's Phone then leave this to true else put false
 Config.Phone = 'qb-phone' -- If using Renewed's Phone then put your resource name here
-Config.NotificationStyle = 'phone'
+Config.OnlyLeader = true -- Allow only the leader of the group to sign into the tow job [RECOMMENDED: TRUE]
+Config.GroupLimit = 2
 
 Config.Job = 'tow'
 Config.JobGrade = 0
-
-Config.LaptopCoords = vector3(471.58, -1310.89, 28.94)
-Config.GroupLimit = 2
-
 Config.DepositRequired = true
 Config.DepositAmount = 100
 
-Config.TowTruck = 'flatbed'
+Config.LaptopCoords = vector3(471.58, -1310.89, 28.94)
 Config.VehicleSpawns = {
     vector4(499.06, -1333.53, 29.42, 26.54),
     vector4(486.88, -1332.38, 29.38, 297.66),
