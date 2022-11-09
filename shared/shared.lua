35047420aafbdd98c70b7f59b185ac140b703851
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-11-08 23:21:07 -0500

    base config

---
+++ shared/shared.lua
@@ -0,0 +1,23 @@
+Config = Config or {}
+
+Config.Core = 'qb-core'
+Config.Target = 'qb-target'
+Config.Fuel = 'LegacyFuel'
+
+Config.RenewedPhone = true -- If you use Renewed's Phone then leave this to true else put false
+Config.Phone = 'qb-phone' -- If using Renewed's Phone then put your resource name here
+Config.NotificationStyle = 'phone'
+
+Config.LaptopCoords = vector3(471.58, -1310.89, 28.94)
+Config.GroupLimit = 2
+
+Config.DepositRequired = true
+Config.DepositAmount = 100
+
+Config.TowTruck = 'flatbed'
+Config.VehicleSpawns = {
+    vector4(499.06, -1333.53, 29.42, 26.54),
+    vector4(486.88, -1332.38, 29.38, 297.66),
+    vector4(496.51, -1332.84, 29.42, 3.8),
+    vector4(492.45, -1331.87, 29.41, 344.83)
+}
